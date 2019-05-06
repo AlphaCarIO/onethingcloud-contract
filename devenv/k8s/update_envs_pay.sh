dd950dc4a0d1404a5127ed386117585874fe3192
lchan <leochan007@163.com>
2019-05-06 09:26:42 +0800

    update yaml file of wallet for pay.

---
+++ devenv/k8s/update_envs_pay.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+FOLDER=test
+
+TMP_CMD=
+
+NS='--namespace onethingcloud'
+
+if [ -n "$1" ]; then
+    FOLDER=$1
+fi
+
+if [ xt == x"$2" ]; then
+    TMP_CMD=--insecure-skip-tls-verify=true
+fi
+
+echo '---> NS:'$NS
+
+if [ "prod" == "$FOLDER" ]; then
+  echo 'set-context acar-credit-prod...'
+  kubectl config use-context acar-credit-prod
+else
+  echo 'set-context acar-test-env...'
+  kubectl config use-context acar-test-env
+fi
+
+cd ./envs/$FOLDER/
+
+kubectl $TMP_CMD delete secret passwd-pay.json $NS
+kubectl $TMP_CMD create secret generic \
+  --from-file=passwd.json=passwd-pay.json passwd-pay.json $NS
+
+kubectl $TMP_CMD delete secret private-key-pay $NS
+kubectl $TMP_CMD create secret generic \
+  --from-file=private_key=private_key-pay private-key-pay $NS
